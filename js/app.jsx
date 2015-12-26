cb4efbca1141de901aa62a595479d8d0ec9a22aa
Kelvin Yang <hanjayyang@gmail.com>
2015-12-26 14:45:53 +0800

    First commit.

---
+++ js/app.jsx
@@ -0,0 +1,10 @@
+var React = require('react/addons');
+var ReactDOM = require('react-dom');
+var Timeline = require('./Timeline.jsx');
+//var mockEvents = require('../mock/real_mock.js')
+
+ReactDOM.render(
+    <Timeline/>,
+    document.getElementById('algo-dom')
+); 
+
